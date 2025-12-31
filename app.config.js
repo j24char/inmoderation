a81a6c9db23b86b4e4a2f64019325d7ebf76b8ed
j24char <justinacharron@gmail.com>
2025-12-30 20:59:22 -0600

    Updated version to 1.0.1

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "InModeration",
     slug: "InModeration",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     assetBundlePatterns: ["**/*"],
